7b1bbed68bbec96982cfbc5402beba3f9d64fff6
ankitit <40754682+ankitit@users.noreply.github.com>
2021-11-19 07:48:01 +0530

    test

--- infra-as-code/was-ec2-instance/main.tf
+++ infra-as-code/was-ec2-instance/main.tf
@@ -1,17 +1,51 @@
-terraform {
-  required_version = ">= 0.11.0"
-}
-
+#specify the provider & access details 
 provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
+provider "archive" {}
 
-  tags {
-    Name = "${var.name}"
+data "archive_file" "zip"{
+  type        = "zip"
+  source_file =  "hello_lambda.py"
+  output_path = "hell0_lambda.zip"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "service"
+
   }
+   actions = ["sts:AssumeRole"]
+
+  }
+}
+
+resource "aws_iam_role" "iam_role_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+  
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "hello_lambda"
+
+  filename = "${data.archive_file.zip.output_path}"
+  source_code_hash = "${data.archive_file.zip.output_base64hash256}"
+
+  role = "${aws_iam_role.iam_for_lambda.arn}"
+  handler = "hello_lambda.lambda_handler"
+  runtime = "python3.6"
+
+  environment {
+    variables = {
+      greting ="Ankithere"
+    }
+  }
+
 }
